9526122ab49268e0810cc737482040447c579200
geeky-hypertext629 <107639960+geeky-hypertext629@users.noreply.github.com>
2023-08-05 00:35:12 +0530

    Setup basic config.env

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const {PORT}  = require('./config/serverConfig');
+
+const setupAndStartServer = async () =>{
+    //create the express object
+    const app = express();
+
+    // const PORT = 3000
+    app.listen(PORT,()=>{
+        console.log(`Server started on port ${PORT}`); // String Interpolation
+        
+    })
+}
+
+
+setupAndStartServer();
